55dc286204d697fbeb99cbde02eeac1ab5fbaa33
megh-bari <meghbari01@gmail.com>
2024-06-24 19:35:40 +0530

    lec 3 done

---
+++ Lec 3 - DataTypes/datatype.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+// code redability
+console.log(3 + 3);
+
+console.log("Megh");
+
+// DataTypes
+
+let name = "Megh"; //String dataTypes
+let age = 18; // number
+let isLoggedIn = false; // boolean dataTypes
+let state = null;
+
+/* Premative DataTypes*/
+// number => 2 to power of 53
+// bigint => not that much required
+// string => " " or ' '
+// boolean => true or false
+// null => StandAlone value (empty)
+// undefined =>
+// symbol => unique
+
+/* Object */
+
+console.log(typeof "megh"); // string
+console.log(typeof age); // Number
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
