903eae7ca501bba6f30050598774a478eb225873
megh-bari <meghbari01@gmail.com>
2024-06-25 17:53:01 +0530

    Lec 5 done

---
+++ Lec  5 - Operations/operation.js
@@ -0,0 +1,46 @@
+let value = 18;
+let negativeValue = -value;
+
+// console.log(negativeValue);
+
+/* Arithmatic Operation */
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+*/
+
+
+let str1 = "Hello"
+let str2 = " Megh"
+let str3 = str1 + str2
+
+console.log(str3);
+
+//  Conversion 
+// This are bad way to writing code
+console.log(1+"2");
+
+console.log("1" + 2 + 2);
+
+console.log(1 + 2 + "2");
+
+console.log(true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+
+// console.log(num1);
+
+let gameCounter = 100
+
+gameCounter++
+
+
+console.log(gameCounter);
