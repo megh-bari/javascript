6567c71ac76d329881aa18573649eb163c818a2d
megh-bari <meghbari01@gmail.com>
2024-06-25 14:37:10 +0530

    Lec 4 done

---
+++ Lec 4 - DataType Conversion/conversion.js
@@ -0,0 +1,36 @@
+let score = "megh"
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// 33 => number
+
+// Boolean Conversion
+let isLoggedin = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+// 1 =>  true; 0 => false
+// "" => false
+// "megh" => true
+
+
+
+// String Conversion
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
