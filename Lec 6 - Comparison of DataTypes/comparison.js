d0a0ae3aabc326afa7d868f9d333034e4d415333
megh-bari <meghbari01@gmail.com>
2024-06-25 18:07:44 +0530

    Lec 6 done

---
+++ Lec 6 - Comparison of DataTypes/comparison.js
@@ -0,0 +1,36 @@
+
+// Basic Comparison 
+
+// console.log(2>1);
+
+// console.log(2>=1);
+
+// console.log(2<1);
+
+// console.log(2<=1);
+
+// console.log(2==1);
+
+// console.log(2!=1);
+
+
+// console.log("2">1);
+
+// console.log("02">1);
+
+
+console.log(null>0);
+
+console.log(null>=0);
+// above code is true because the > (comparasion) convert the null to a number treating as 0, and == equality check 
+
+console.log(null==0);
+
+
+// always false
+console.log(undefined==0);
+
+
+// === (strict check)
+
+console.log("2"===2);
