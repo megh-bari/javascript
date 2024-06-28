48d2031c867e7f163d9b54c6930711467724bb9a
megh-bari <meghbari01@gmail.com>
2024-06-28 21:15:13 +0530

    Number and Math

---
+++ Lec 9 - Number & Math/math.js
@@ -0,0 +1,22 @@
+console.log(Math)
+
+console.log(Math.abs(-4)) // negative to positive
+
+console.log(Math.round(3.14))
+console.log(Math.ceil(10.3)) // top value
+console.log(Math.floor(12.3)) // ground value 
+
+console.log(Math.sqrt(25))
+
+console.log(Math.min(2, 34, 1, 222))
+console.log(Math.max(100, 23, 232,1000))
+
+// console.log(Math.random())
+// console.log(Math.floor(Math.random()*10) + 1)
+
+
+// Random Number Generate formula
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // formula
---
+++ Lec 9 - Number & Math/num.js
@@ -0,0 +1,19 @@
+const score = 100
+
+console.log(score)
+
+const balance = new Number(500)
+console.log(balance)
+
+console.log(balance.toString())
+// console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4))
+
+const num = 1000000
+
+console.log(num.toLocaleString('en-IN'))
+
