b41fa8b095bf08b1555351989c4e182deb2f64a5
megh-bari <meghbari01@gmail.com>
2024-07-03 20:45:53 +0530

    object done

---
+++ Lec 12 - Objects/objects-p3.js
@@ -0,0 +1,37 @@
+// de-structure of Object
+
+const course = {
+    coursename: "JS",
+    price: "999",
+    instructor: "chai or code"
+}
+
+// console.log(course.instructor)
+// or
+
+const {instructor, price} = course
+console.log(instructor, price)
+
+
+// JSON API
+
+// {
+            // this is JSON
+// }
+
+// {
+//     'name': 'Megh',
+//     'id': '123@abc',
+//     'pass': '123456'
+// }
+
+[
+    {},
+    {},
+    {},
+    {}
+]
+
+// Random User me api
+
+// https://randomuser.me/api/
