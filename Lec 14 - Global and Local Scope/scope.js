f3670f226e2089bf6784fd805ca5f51018b914e1
megh-bari <meghbari01@gmail.com>
2024-07-06 20:27:11 +0530

    Scope level and mini hoisting

--- Lec 14 - Global and Local Scope/scope.js
+++ Lec 14 - Global and Local Scope/scope.js
@@ -6,11 +6,58 @@ let a = 300 // global scope
 if (true) {
   let a = 10;
   const b = 20;
-  console.log("Inner",a) // block scope
+//   console.log("Inner",a) // block scope
 }
 
 // if ke andar ka content is block scope
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// nested structure scope
+
+function funcOne() {
+    const userName = "Megh"
+
+    function funcTwo() {
+        const website = "Youtube"
+        console.log(userName)
+    }
+    // console.log(website)
+
+    funcTwo()
+}
+
+// funcOne()
+
+if (true) {
+   const userName = "megh" 
+   if (userName === "megh") {
+    const website = " Youtube"
+    // console.log(userName + website)
+   }
+//    console.log(website)
+} 
+
+// console.log(userName)
+
+
+// ++++++++++++++++++++++++++++++ intersting +++++++++++++++++++++++++++
+
+console.log(addOne(5))
+function addOne(num) {
+    return num +1
+}
+
+
+
+//  Or
+
+console.log(addTwo(5))
+const addTwo = function(num) {
+    return num +2
+}
+
+
