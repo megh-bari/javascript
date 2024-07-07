3ae964a7a5bcfa6549b1b6a467002b0a17876bcc
megh-bari <meghbari01@gmail.com>
2024-07-07 18:39:29 +0530

    Immediately Invoked Function Expression

---
+++ Lec 16 - IIFE/iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression
+
+(function code() {
+    // named IIFE
+  console.log("DB Connected!");
+})();
+
+// syntax:
+// (function defination )
+// (exicution)
+
+((name) => {
+  console.log(`${name}, welcome to database`);
+})("Megh");
