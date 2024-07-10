2367118de3c604e092d4b3bcf6c189ea24ed0394
megh-bari <meghbari01@gmail.com>
2024-07-10 20:32:20 +0530

    loops in js

---
+++ Lec 19 - Loops/do-while-loop.js
@@ -0,0 +1,10 @@
+// * do while
+
+// ! Atleast run once
+
+let score = 1;
+
+do {
+  console.log(`The score is: ${score}`);
+  score++;
+} while (score <= 10);
---
+++ Lec 19 - Loops/while-loop.js
@@ -0,0 +1,16 @@
+// * While Loop 
+
+let i = 1
+
+while (i<=10) {
+    // console.log(i)
+    i = i+2
+}
+
+let myArr = ['batman', 'flying jaat', 'spiderman']
+let arr = 0
+
+while (arr<myArr.length) {
+    console.log(`${myArr[arr]}`)
+    arr++
+}
