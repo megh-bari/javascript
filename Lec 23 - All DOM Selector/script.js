52d4109035f5debc2114148d9db38b2bf4bd0d5e
megh-bari <meghbari01@gmail.com>
2024-07-13 15:24:31 +0530

    All DOM selectors NodeList and HTMLCollection

---
+++ Lec 23 - All DOM Selector/script.js
@@ -0,0 +1,41 @@
+// document.getElementById('title')
+// document.getElementById('title').id
+// document.getElementById('title').class
+// document.getElementById('title').className
+// document.getElementById('title').getAttribute
+// document.getElementById('title').getAttribute('id')
+// document.getElementById('title').getAttribute('class')
+
+
+// document.getElementById('title').setAttribute("class","test")
+
+// !!! variable holding
+// const title = document.getElementById('title')
+// title.style.backgroundColor = "yellow"
+// title.style.borderRadius = "20px"
+
+
+// title
+// title.innerHTML
+// title.innerText
+
+
+
+// document.querySelector('h2')
+
+
+// !!! converting html collection to nodelist
+
+// document.getElementsByClassName('list')
+
+// const list = document.getElementsByClassName('list')
+
+// list
+
+// Array.from(list)
+
+// const myConvertedList = Array.from(list)
+
+// myConvertedList.forEach(function(list) {
+//     list.style.backgroundColor = "yellow"
+// })
